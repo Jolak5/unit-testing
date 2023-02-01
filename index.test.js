d5316a773f569ebe6d45e94d54bfc13f6e8ebd82
jolak5 <kayodej4@gmail.com>
2023-02-01 08:06:36 +0100

    Add more tests to index.js

--- index.test.js
+++ index.test.js
@@ -7,3 +7,11 @@ const stringLength = require('./index.js');
 it('get length of string', () => {
   expect(stringLength('kayode')).toBe(6);
 });
+
+it('greater than', () => {
+  expect(stringLength('kayode')).toBeGreaterThanOrEqual(1);
+});
+
+it('greater than', () => {
+  expect(stringLength('kayode')).toBeLessThanOrEqual(10);
+});
